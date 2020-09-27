cddf9c22068a53b58c44598f259bd943af348110
Atanas <55555430+ultraasi-atanas@users.noreply.github.com>
2020-09-27 21:26:58 +0100

    Create explained-pixels-from-image-map.js

---
+++ explained-pixels-from-image-map.js
@@ -0,0 +1,45 @@
+/* 
+A p5.js sketch created on the www.openprocessing.org platform. 
+Link to sketch
+https://www.openprocessing.org/sketch/929637
+My profile link 
+https://www.openprocessing.org/user/223901/#sketches
+*/
+
+let img;
+
+function preload() {
+	img = loadImage('test.png');
+}
+
+function setup() {
+	createCanvas(800, 600);
+	noStroke();
+	background(255);
+
+	//prepare image ready to use with img.get(x, y)
+	img.loadPixels();
+}
+
+function draw() {
+	//draw 100 circles using colours drawn from the image (not shown)
+	for (let i = 0; i < 100; i++) {
+		
+		let imgSourceX = floor(random(img.width));
+		let imgSourceY = floor(random(img.height));
+		
+		let colorAtPixel = img.get(imgSourceX, imgSourceY);
+		fill(colorAtPixel);
+		
+		//Mapping the image width & height to the size of the canvas
+		let canvasDestinationX = map(imgSourceX, 0, img.width, 0, width);
+		let canvasDestinationY = map(imgSourceY, 0, img.height, 0, height);
+		
+		// DRAW THE BIGGER IMAGE
+		circle(canvasDestinationX, canvasDestinationY, 1);
+
+		// DRAW THE ORIGINAL IMAGE
+		circle(imgSourceX, imgSourceY, 1);
+
+	}
+}
